ebcac620a52f432d670b19e8ac446b773af509da
Michael Mouchous <michaelm11188@gmail.com>
2017-12-24 18:47:30 +0100

    Holdoff consideration

--- homebridge-contact-gpio-sensor/index.js
+++ homebridge-contact-gpio-sensor/index.js
@@ -6,52 +6,63 @@ var inherits = require('util').inherits;
 var exec = require('child_process').exec;
 
 var RETRY_COUNT = 10;
+var HOLDOFF_MS = 1000;
 
 function translate(value, contactValue) {
-    var val;
     if (value === contactValue) {
         // circuit is closed
-        val = Characteristic.ContactSensorState.CONTACT_DETECTED;
+        return Characteristic.ContactSensorState.CONTACT_DETECTED;
     } else {
         // circuit is open
-        val = Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
+        return Characteristic.ContactSensorState.CONTACT_NOT_DETECTED;
+    }
+}
+
+function forceRead(retryCount, gpio) {
+    var val;
+    for (var i = 0; i < retryCount; i++) {
+        val = gpio.readSync();
+        if (typeof val == 'number') {
+            break;
+        }
     }
     return val;
 }
-
 
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
     HomebridgeAPI = homebridge;
-    
+
     //console.log(Service.ContactSensor);
     homebridge.registerAccessory("homebridge-contact-gpio-sensor", "ContactGPIOSensor", ContactGPIOSensor);
 };
 
 function ContactGPIOSensor(log, config) {
-    var that = this;
+    var self = this;
     this.log = log;
     this.name = config.name;
     this.pinId = config.BCM_GPIO;
     this.retryCount = config.retryCount || RETRY_COUNT;
-    
+    this.holdoffMS = config.holdoffMS || HOLDOFF_MS;
+
     log(config.contactValue)
     if (typeof config.contactValue == 'number') {
         this.contactValue = config.contactValue;
     } else {
         this.contactValue = 1;
     }
-    
+
     exec('gpio -g mode ' + this.pinId + ' up', function (error, stdout, stderr) {
          if (error !== null) {
          log('exec error: ' + error);
          }
          });
-    
+
     this.contactSensor = new Gpio(this.pinId, 'in', 'both');
-    this.openedCounter = 0;
-    
+
+    // To count the number of times the contact has been opened
+    this.changesCounter = 0;
     TimesOpened = function () {
         Characteristic.call(this, 'Times Opened', 'E863F129-079E-48FF-8F27-9C2605A29F52');
         this.setProps({
@@ -64,46 +75,71 @@ function ContactGPIOSensor(log, config) {
     };
     inherits(TimesOpened, Characteristic);
     TimesOpened.UUID = 'E863F129-079E-48FF-8F27-9C2605A29F52';
-    
     this.characteristic_times_opened = TimesOpened;
-    
+
     // Contact Sensor Service
     this.service = new Service.ContactSensor(this.name);
     this.service.addOptionalCharacteristic(TimesOpened);
-    
+
+    // Binding Characteristics
     this.service.getCharacteristic(Characteristic.ContactSensorState)
-    .on('get', this.getState.bind(this));
-    
+      .on('get', this.getState.bind(this));
     this.service.getCharacteristic(TimesOpened)
-    .on('get', this.getOpenedCounter.bind(this));
-    
-    this.contactSensor.watch(function(err, value) {
-        that.openedCounter++;
-        that.log('opened counter ' + that.openedCounter);
-        that.service.getCharacteristic(that.characteristic_times_opened)
-             .setValue(Math.floor(that.openedCounter/2));
-        that.service.getCharacteristic(Characteristic.ContactSensorState)
-             .setValue(translate(value, that.contactValue));
-    });
-    
+      .on('get', this.getTimesOpened.bind(this));
+
+    // To release the GPIO when exiting
     process.on('SIGINT', function () {
-               that.contactSensor.unexport();
-               });
+        self.contactSensor.unexport();
+    });
+
+    this.currentGpioValue = forceRead(this.retryCount, this.contactSensor);
+    this.lastCallerIdentifier = Date.now();
+
+    // Watch any changes events
+    this.contactSensor.watch(function(err, value) {
+      // To prevent glitches, we make delayed calls to processChange
+      // with caller identifier (based on timestamp)
+      // If a change has been observed before scheduled self.processChange has been invoked
+      // the scheduled self.processChange will be invalidated.
+      self.lastCallerIdentifier = Date.now();
+      setTimeout(self.processChange, self.holdoffMS, self.lastCallerIdentifier);
+    }
 }
 
-ContactGPIOSensor.prototype.getState = function(callback) {
-    var val;
-    for (var i = 0; i < this.retryCount; i++) {
-        val = this.contactSensor.readSync();
-        if (typeof val == 'number') {
-            break;
-        }
+// Process changes events
+ContactGPIOSensor.prototype.processChange = function(callerIdentifier) {
+    if (this.lastCallerIdentifier != callerIdentifier) {
+      // Ignore if it is not the last caller identifier
+      this.log("Ignoring because " + callerIdentifier + " is not the last caller identifier " + this.lastCallerIdentifier)
+      return;
     }
-    callback(null, translate(val, this.contactValue));
+
+    // Readback GPIO value
+    gpioValue = forceRead(this.retryCount, this.contactSensor);
+    if (gpioValue == this.currentGpioValue) {
+      // Ok, we consider that it was nothing
+      this.log("Ignoring because " + gpioValue + " does not differ from last value " + this.currentGpioValue);
+      return;
+    }
+
+    // Change! we have to inform the whole world
+    this.changesCounter++;
+    this.currentGpioValue = gpioValue;
+    this.log('Changes counter ' + this.changesCounter);
+    this.service.getCharacteristic(this.characteristic_times_opened)
+         .setValue(Math.floor(this.changesCounter/2));
+    this.service.getCharacteristic(Characteristic.ContactSensorState)
+         .setValue(translate(gpioValue, this.contactValue));
 };
 
-ContactGPIOSensor.prototype.getOpenedCounter = function(callback) {
-    callback(null, Math.floor(this.openedCounter/2));
+ContactGPIOSensor.prototype.getState = function(callback) {
+  // Force reading state
+  var val = forceRead(this.retryCount, this.contactSensor);
+  callback(null, translate(val, this.contactValue));
+};
+
+ContactGPIOSensor.prototype.getTimesOpened = function(callback) {
+    callback(null, Math.floor(this.changesCounter/2));
 };
 
 ContactGPIOSensor.prototype.getServices = function() {
