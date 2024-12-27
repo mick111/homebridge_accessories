81d0c7ecc53e4da1438a850deb36b8f44767b037
Michael Mouchous <michaelm11188@gmail.com>
2024-12-27 17:04:55 +0100

    missing instructions

--- homebridge-portail-double/index.js
+++ homebridge-portail-double/index.js
@@ -2,81 +2,85 @@ var rpio = require("rpio");
 var Service, Characteristic, HomebridgeAPI;
 
 module.exports = function (homebridge) {
-  Service = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  HomebridgeAPI = homebridge;
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    HomebridgeAPI = homebridge;
 
-  // Register accessory
-  homebridge.registerAccessory(
-    "homebridge-portail-double",
-    "PortailDouble",
-    PortailDouble
-  );
+    // Register accessory
+    homebridge.registerAccessory(
+        "homebridge-portail-double",
+        "PortailDouble",
+        PortailDouble
+    );
 };
 
 // Accessory constructor
 class PortailDouble {
-  constructor(log, config) {
-    this.log = log;
-    this.name = config.name;
+    constructor(log, config) {
+        this.log = log;
+        this.name = config.name;
 
-    this.GrandeOuverture_GPIO = config.GrandeOuverture_GPIO;
-    this.PetiteOuverture_GPIO = config.PetiteOuverture_GPIO;
+        this.GrandeOuverture_GPIO = config.GrandeOuverture_GPIO;
+        this.PetiteOuverture_GPIO = config.PetiteOuverture_GPIO;
 
-    // Services instantiations
+        rpio.open(this.GrandeOuverture_GPIO, rpio.OUTPUT, rpio.HIGH);
+        rpio.open(this.PetiteOuverture_GPIO, rpio.OUTPUT, rpio.HIGH);
 
-    // Information
-    this.informationService = new Service.AccessoryInformation();
-    this.informationService
-      .setCharacteristic(Characteristic.Manufacturer, "MM Manufacturer")
-      .setCharacteristic(Characteristic.Model, "MM Model")
-      .setCharacteristic(Characteristic.SerialNumber, "MM SN");
+        // Services instantiations
 
-    // Switch 1
-    this.GrandeOuverture_SwitchService = new Service.Switch(
-      "Grande Ouverture",
-      "button1"
-    );
-    this.GrandeOuverture_SwitchService.getCharacteristic(Characteristic.On)
-      .on("get", this.getGrandeOuverture_SwitchState.bind(this))
-      .on("set", this.setGrandeOuverture_SwitchState.bind(this));
+        // Information
+        this.informationService = new Service.AccessoryInformation();
+        this.informationService
+            .setCharacteristic(Characteristic.Manufacturer, "MM Manufacturer")
+            .setCharacteristic(Characteristic.Model, "MM Model")
+            .setCharacteristic(Characteristic.SerialNumber, "MM SN");
 
-    // Switch 2
-    this.PetiteOuverture_SwitchService = new Service.Switch(
-      "Petite Ouverture",
-      "button2"
-    );
-    this.PetiteOuverture_SwitchService.getCharacteristic(Characteristic.On)
-      .on("get", this.getPetiteOuverture_SwitchState.bind(this))
-      .on("set", this.setPetiteOuverture_SwitchState.bind(this));
-  }
+        // Switch 1
+        this.GrandeOuverture_SwitchService = new Service.Switch(
+            "Grande Ouverture",
+            "button1"
+        );
+        this.GrandeOuverture_SwitchService.getCharacteristic(Characteristic.On)
+            .on("get", this.getGrandeOuverture_SwitchState.bind(this))
+            .on("set", this.setGrandeOuverture_SwitchState.bind(this));
 
-  getGrandeOuverture_SwitchState(callback) {
-    var onCommand = rpio.read(this.GrandeOuverture_GPIO);
-    this.log("Getting Grande ouverture state: %s", onCommand);
-    callback(null, onCommand);
-  }
-  setGrandeOuverture_SwitchState(value, callback) {
-    this.log("Setting Grande ouverture state to %s", value);
-    rpio.write(this.GrandeOuverture_GPIO, value ? rpio.LOW : rpio.HIGH);
-    callback();
-  }
+        // Switch 2
+        this.PetiteOuverture_SwitchService = new Service.Switch(
+            "Petite Ouverture",
+            "button2"
+        );
+        this.PetiteOuverture_SwitchService.getCharacteristic(Characteristic.On)
+            .on("get", this.getPetiteOuverture_SwitchState.bind(this))
+            .on("set", this.setPetiteOuverture_SwitchState.bind(this));
+    }
 
-  getPetiteOuverture_SwitchState(callback) {
-    this.log("Getting Petite ouverture state: %s", onCommand);
-    callback(null, onCommand);
-  }
-  setPetiteOuverture_SwitchState(value, callback) {
-    this.log("Setting Petite ouverture state to %s", value);
-    rpio.write(this.PetiteOuverture_GPIO, value ? rpio.LOW : rpio.HIGH);
-    callback();
-  }
+    getGrandeOuverture_SwitchState(callback) {
+        var onCommand = rpio.read(this.GrandeOuverture_GPIO);
+        this.log("Getting Grande ouverture state: %s", onCommand);
+        callback(null, onCommand);
+    }
+    setGrandeOuverture_SwitchState(value, callback) {
+        this.log("Setting Grande ouverture state to %s", value);
+        rpio.write(this.GrandeOuverture_GPIO, value ? rpio.LOW : rpio.HIGH);
+        callback();
+    }
 
-  getServices() {
-    return [
-      this.informationService,
-      this.GrandeOuverture_SwitchService,
-      this.getPetiteOuverture_SwitchService,
-    ];
-  }
+    getPetiteOuverture_SwitchState(callback) {
+        var onCommand = rpio.read(this.PetiteOuverture_GPIO);
+        this.log("Getting Petite ouverture state: %s", onCommand);
+        callback(null, onCommand);
+    }
+    setPetiteOuverture_SwitchState(value, callback) {
+        this.log("Setting Petite ouverture state to %s", value);
+        rpio.write(this.PetiteOuverture_GPIO, value ? rpio.LOW : rpio.HIGH);
+        callback();
+    }
+
+    getServices() {
+        return [
+            this.informationService,
+            this.GrandeOuverture_SwitchService,
+            this.getPetiteOuverture_SwitchService,
+        ];
+    }
 }
